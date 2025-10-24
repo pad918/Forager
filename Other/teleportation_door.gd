472e6a0edc51bef679619ac79872b2d9d3cfd871
Måns Abrahamsson <mans.abrahamsson@gmail.com>
2025-10-24 16:12:53 +0200

    Teleportation doors

---
+++ Other/teleportation_door.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+class_name TeleportationDoor
+
+@export var area: Area2D
+@export var destination: Node2D
+
+
+var player:Player = null
+
+func _ready() -> void:
+	area.area_entered.connect(
+		func(a:Area2D):
+			player = a.get_parent()
+	)
+	area.area_exited.connect(
+		func(a:Area2D):
+			player = null
+	)
+	
+func _input(event: InputEvent) -> void:
+	if player!=null and Input.is_key_pressed(KEY_ENTER):
+		player.position = destination.position
